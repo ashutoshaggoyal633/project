27c50276fb307fda98d1b8c7f3a16b92a5a77b35
Ashutosh Goyal <76815693+ashutoshaggoyal633@users.noreply.github.com>
2021-06-12 23:51:40 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "cms";
+ $conn = mysqli_connect($servername,$username,$password,$dbname);
+ if(@conn)
+	 echo "Connection Est";
+ else 
+	 echo "Connection not Est";
+ ?>
